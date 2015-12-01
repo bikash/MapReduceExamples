a46b58824f89885673f2e800d486054d61b3f3da
Bikash <er.bikash21@gmail.com>
2015-12-01 01:08:57 -0500

    string manipulation to get base key

--- src/com/agarsofttech/mr/GACombiner.java
+++ src/com/agarsofttech/mr/GACombiner.java
@@ -2,6 +2,7 @@ package com.agarsofttech.mr;
 
 import java.io.IOException;
 
+
 //import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -9,7 +10,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class GACombiner
        extends Reducer<Text,PairOfFloatString,Text,PairOfFloatString> {
     //private FloatWritable result = new FloatWritable();
-
     public void reduce(Text key, Iterable<PairOfFloatString> values,
                        Context context
                        ) throws IOException, InterruptedException {
@@ -21,9 +21,22 @@ public class GACombiner
       PairOfFloatString kv = new PairOfFloatString();
       String k = key.toString();
       kv.set(sum, k);
-      System.out.println("Key > sum" + kv.toString());
+      Text BaseKey = new Text(getBaseKey(k));
+      System.out.println("Key > " + BaseKey);
+      //Convert string of [38, 39, 48] in to 38 as key
+      //System.out.println("Key > sum" + kv.toString());
       //result.set(sum);
-      context.write(key, kv);
+      context.write(BaseKey, kv);
 
     }
+    
+    public String getBaseKey(String k)
+    {
+    	//System.out.println("Key in > " + k);
+    	String[] arr = k.split(",");
+    	String a = arr[0].substring(1, arr[0].length());
+    	//System.out.println("Key > " + arr[0] + "  "+a);
+		return a;
+    }
+    
   }
--- src/com/agarsofttech/mr/SPGATry1.java
+++ src/com/agarsofttech/mr/SPGATry1.java
@@ -25,15 +25,15 @@ public static void main(String[] args) throws Exception {
 
     //job.setReducerClass(GAReducer.class);
     //job.setMapperClass(MaxFitMapper.class);
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(PairOfFloatString.class);
+    //job.setMapOutputKeyClass(Text.class);
+    //job.setMapOutputValueClass(PairOfFloatString.class);
     
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(PairOfFloatString.class);
     //FileInputFormat.addInputPath(job, new Path(args[0]));
     //FileOutputFormat.setOutputPath(job, new Path(args[1]));
     FileInputFormat.addInputPath(job, new Path("data/input1.txt"));
-    FileOutputFormat.setOutputPath(job, new Path("out1"));
+    FileOutputFormat.setOutputPath(job, new Path("out332"));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
